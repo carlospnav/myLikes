d449d1631b7d9d0ca9611ebedf71e5c78561ec11
Carlos P Navarrete Filho <karnav@gmail.com>
2017-07-26 10:50:54 -0300

    Update: SearchBooks search query now updates query state.

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class SearchBooks extends Component{
+
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query })
+  }
+
+  onSearchQuery = (event) => {
+    console.log(event.target.value);
+    this.updateQuery(event.target.value)
+   }
+
   render(){
     return(
       <div className="search-books">
@@ -16,7 +30,7 @@ class SearchBooks extends Component{
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author"/>
+            <input value={ this.state.query } onChange={ this.onSearchQuery } type="text" placeholder="Search by title or author"/>
             
           </div>
         </div>
